af4bd33f00914ee9776953142b15afc692254456
Asia <dobrowolska.krakow@gmail.com>
2023-10-08 13:33:30 +0200

    made whole game

--- index.js
+++ index.js
@@ -1,27 +1,96 @@
 'use strict';
-const tile = document.querySelectorAll('.tile');
+const tiles = document.querySelectorAll('.tile');
 const btnStart = document.querySelector('.start');
+const btnRemove = document.querySelector('.reset');
+const timeCounter = document.querySelector('.timeCounter');
+const livesCounter = document.querySelector('.liveCounter');
+const punksCounter = document.querySelector('.punksCounter');
 
 let currentTile = null;
+let interval = null;
+let timeInterval = null;
+let endTimeTimeout = null;
+let time = 60;
+let randomTile;
+let tileClicked = false;
+let lives = 3;
+let punks = 0;
+
+livesCounter.innerHTML = lives;
+punksCounter.innerHTML = punks;
 
 const onStart = () => {
-  let randomTile;
-
-  const interval = setInterval(() => {
+  interval = setInterval(() => {
     if (currentTile) {
       currentTile.classList.remove('active');
     }
-    randomTile = tile[Math.floor(Math.random() * tile.length)];
+    tileClicked = false;
+    randomTile = tiles[Math.floor(Math.random() * tiles.length)];
     randomTile.classList.add('active');
+
+    setTimeout(() => {
+      randomTile.classList.remove('active');
+      if (!tileClicked) {
+        lives--;
+        livesCounter.innerHTML = lives;
+        alert('Straciłeś życie!');
+        if (lives === 0) {
+          alert('Straciłeś wszystkie życia. Koniec gry!');
+          clear();
+        }
+      }
+    }, 2000);
+
     currentTile = randomTile;
   }, 3000);
 
-  setTimeout(() => {
-    clearInterval(interval);
-    if (randomTile) {
-      randomTile.classList.remove('active');
-    }
+  timeInterval = setInterval(() => {
+    time--;
+    timeCounter.innerHTML = time;
+  }, 1000);
+
+  endTimeTimeout = setTimeout(() => {
+    clear();
+    alert('Koniec czasu! Zacznij nową grę');
   }, 60000);
 };
 
+const chooseTile = e => {
+  if (!e.target.classList.contains('active')) {
+    return;
+  }
+
+  if (e.target !== currentTile) {
+    lives--;
+    alert('Straciłeś życie!');
+    livesCounter.innerHTML = lives;
+
+    if (lives === 0) {
+      alert('Straciłeś wszystkie życia. Koniec gry!');
+      clear();
+    }
+  } else {
+    punks++;
+    tileClicked = true;
+    e.target.classList.remove('active');
+    punksCounter.innerHTML = punks;
+  }
+};
+
+const clear = () => {
+  clearInterval(interval);
+  tiles.forEach(tile => tile.classList.remove('active'));
+  currentTile = null;
+  clearInterval(timeInterval);
+  clearTimeout(endTimeTimeout);
+  time = 60;
+  timeCounter.innerHTML = '';
+  lives = 3;
+  livesCounter.innerHTML = lives;
+  punks = 0;
+  punksCounter.innerHTML = punks;
+};
+
 btnStart.addEventListener('click', onStart);
+btnRemove.addEventListener('click', clear);
+tiles.forEach(tile => tile.addEventListener('click', chooseTile));
